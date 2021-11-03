502be39a4277c646fe7ecb645dc609435527f487
Diego Roble <diego.roble@zabud.com.co>
2021-11-02 21:20:25 -0500

    modificando el front

--- crud-carros-django/proyecto/proyecto_init/templates/static/js/app.js
+++ crud-carros-django/proyecto/proyecto_init/templates/static/js/app.js
@@ -1,8 +1,3 @@
-const nombres = document.querySelector('#id_nombres');
-const apellidos = document.querySelector('#id_apellidos');
-const telefomo = document.querySelector('#id_telefono');
-const correo = document.querySelector('#id_correo');
-nombres.classList = "form-control";
-apellidos.classList = "form-control";
-telefomo.classList = "form-control";
-correo.classList = "form-control";
+const inputsList = document.querySelectorAll('form input');
+
+inputsList.forEach(elemento => elemento.classList = "form-control");
